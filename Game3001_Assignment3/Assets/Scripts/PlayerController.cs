398c9558b898a87f1c07ef5e0025fb49ee7aa5c7
NotAdamJabbar <Adam.alabduljabbar@gmail.com>
2024-04-13 12:55:28 -0400

    Assignment start

---
+++ Game3001_Assignment3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0||Input.GetAxisRaw("Vertical")!=0)
+        {
+            transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized*moveSpeed*Time.deltaTime);
+        }
+    }
+}
