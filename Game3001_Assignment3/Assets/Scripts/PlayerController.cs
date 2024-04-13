18a129608b042b3820ac6c2cc3402816cbea0873
NotAdamJabbar <Adam.alabduljabbar@gmail.com>
2024-04-13 13:10:48 -0400

    player movement added

--- Game3001_Assignment3/Assets/Scripts/PlayerController.cs
+++ Game3001_Assignment3/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+    Vector2 movement;
+    Rigidbody2D rb;
 
-    void Update()
+    private void Start()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0||Input.GetAxisRaw("Vertical")!=0)
-        {
-            transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized*moveSpeed*Time.deltaTime);
-        }
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
 
+    void FixedUpdate()
+    {
+        Move();
+        RotateTowardsMousePointer();
+    }
+
+    private void Move()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (movement.magnitude > 1)
+            movement = movement.normalized;
+
+        rb.AddForce(movement * moveSpeed);
+    }
+
+    void RotateTowardsMousePointer()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f ;
+        rb.rotation = angle;
     }
 }
